0f26c32d451de999c203a181a478f76c03206927
kbroo <kbroo@example.com>
2026-01-18 21:29:06 -0800

    Force Strava authorization prompt

--- app/api/auth/strava/route.ts
+++ app/api/auth/strava/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     const redirectUri = process.env.STRAVA_REDIRECT_URI
     const scope = 'read,activity:read_all'
 
-    const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`
+    const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&approval_prompt=force`
 
     return NextResponse.redirect(authUrl)
 }
